cda99fbec43b746e20688eea2807b232d9c54c9c
Susumu Shimonishi <charlie.shimonishi.uwg@gmail.com>
2021-08-05 12:53:13 +0900

    🌱 タイトル追加・リスト表示コンポーネントの骨格を作成。

--- src/components/addTitle.tsx
+++ src/components/addTitle.tsx
@@ -4,3 +4,116 @@ import Image from "next/image";
 import { Fragment, useCallback, useState, VFC } from "react";
 
 import { client } from "src/libs/supabase";
+
+type props = {
+  uuid: string;
+  getTitleList: VoidFunction;
+};
+
+export const AddTitle: VFC<props> = (props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
+
+  //ダイアログを開く
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  //ダイアログを閉じる
+  const closeModal = useCallback(() => {
+    setTitle("");
+    setAuthor("");
+    setIsOpen(false);
+  }, []);
+
+  //漫画タイトルの追加
+  const handleAdd = useCallback(
+    async (uuid: string) => {
+      if (title == "") {
+        alert("タイトルを入力してください");
+        return;
+      }
+      const { data, error } = await client
+        .from("manga_title")
+        .insert([{ user_id: uuid, title: title, author: author }]);
+      if (error) {
+        alert(error);
+      } else {
+        if (data) {
+          props.getTitleList();
+          closeModal();
+        }
+      }
+    },
+    [title, author, props, closeModal]
+  );
+
+  return (
+    <>
+      <div>
+        <div>ADD NEW</div>
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 z-10 overflow-auto"
+            onClose={closeModal}
+          >
+            <div>
+              <span>&#8203;</span>
+              <Transition.Child as={Fragment} enter="ease-out duration-300">
+                <div>
+                  <Dialog.Title>Add Title</Dialog.Title>
+                  <div>
+                    <input
+                      type="text"
+                      value={title}
+                      onChange={(e) => {
+                        return setTitle(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <div>Author</div>
+                    <input
+                      type="text"
+                      value={author}
+                      onChange={(e) => {
+                        return setAuthor(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <div>
+                      <Button
+                        block
+                        type="default"
+                        size="large"
+                        icon={<IconX />}
+                        onClick={closeModal}
+                      >
+                        Cncel
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        block
+                        size="large"
+                        icon={<IconPlus />}
+                        onClick={() => {
+                          handleAdd(props.uuid);
+                        }}
+                      >
+                        Add
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition>
+      </div>
+    </>
+  );
+};
---
+++ src/components/titleList.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AddTitle } from "src/components/addTitle";
+
+export type Title = {
+  id: number;
+  user_id: string;
+  title: string;
+  author: string;
+  image_url: string;
+};
+
+type TitlesProps = {
+  titles: Title[];
+  uuid: string;
+  getTitleList: VoidFunction;
+  filterText: string;
+};
+
+export const TitleList = (props: TitlesProps) => {
+  //漫画タイトルのフィルタリング
+  const filteredTitle = props.titles.filter((title) => {
+    let searchContent = title.title + " " + title.author;
+    return searchContent.toLowerCase().includes(props.filterText.toLowerCase());
+  });
+
+  return (
+    <div>
+      <AddTitle uuid={props.uuid} getTitleList={props.getTitleList} />
+      {filteredTitle.map((title) => {
+        return (
+          <Link key={title.id} href={`/title?id=${title.id}`} passHref>
+            <div>
+              {title.image_url ? (
+                <Image
+                  src={title.image_url}
+                  alt="thumbnale"
+                  width={126}
+                  height={200}
+                />
+              ) : (
+                <Image src={noImage} alt="thumbnale" width={126} height={200} />
+              )}
+            </div>
+            <div>{title.title}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
